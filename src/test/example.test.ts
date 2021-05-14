dc8125f2aaf6afa1add93e931fe63d0222fd229b
Rob Baile <robert.baile@cazoo.co.uk>
2021-05-14 14:58:18 +0100

    initial commit

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -4,5 +4,6 @@ describe('example test', () => {
     it("should give sum of a + b when adding a and b", () => {
         let example: Example = new Example();
         expect(example.add(1, 2)).toBe(3);
+        // Hello
     })
 })
